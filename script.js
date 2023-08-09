db7dff338f6da94c8cc9224edfb17d7b54d13928
sarahwaithera <140596494+sarahwaithera@users.noreply.github.com>
2023-08-09 12:15:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var h2 = document.querySelector("h2");
+h2.style.color = 'blue'
+h2.style.fontSize = '30px'
+var a = document.querySelector("a")
+var body = document.querySelector("body")
+body.style.backgroundColor = 'aqua'
+var h3 = document.querySelector("h3")
+h3.style.fontWeight = '900'
+h3.style.fontSize = '30px'
+h3.style.textAlign = 'center'
+h3 .style. backgroundColor = 'grey'
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
